9026de0312943071a6a52a3188486dfb8f592d4f
罗杰尹 <luojieyinoffical@126.com>
2024-07-14 20:09:06 +0800

    增加style测试项目

---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            // Get the current button.
+            Button cmd = (Button)e.OriginalSource;
+
+            // Create an instance of the window named
+            // by the current button.
+            Type type = this.GetType();
+            Assembly assembly = type.Assembly;
+            Window win = (Window)assembly.CreateInstance(
+                type.Namespace + "." + cmd.Content);
+
+            // Show the window.
+            win.ShowDialog();
+        }
+    }
+}
