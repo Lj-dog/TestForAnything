728cc9d242871738bf4c052665165c631cdedfe8
Luojieyin <luojieyinoffical@126.com>
2024-07-18 09:10:44 +0800

    24/07/18

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -16,12 +19,26 @@ namespace WpfApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window,INotifyPropertyChanged
     {
+       public ObservableCollection<Student> Students { get { return students; } set { students = value; RaisePropertyChangeed(nameof(Students)); } }
+
+        public void RaisePropertyChangeed(string propertyName)
+        {
+            if(null != this.PropertyChanged)
+            {
+                this.PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        ObservableCollection<Student> students;
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
         }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
 
         private void ButtonClick(object sender, RoutedEventArgs e)
         {
@@ -38,5 +55,32 @@ namespace WpfApp
             // Show the window.
             win.ShowDialog();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Students = new ObservableCollection<Student>
+            {
+                new Student { Name = "张三", Age = 20 },
+                new Student { Name = "李四", Age = 22 },
+                new Student { Name = "王五", Age = 24 }
+            };
+            //studentListBox.ItemsSource = students;
+        }
+
+        private void buttonVictore_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            DependencyObject level1 = VisualTreeHelper.GetParent(btn);
+            DependencyObject level2 = VisualTreeHelper.GetParent(level1);
+            DependencyObject level3 = VisualTreeHelper.GetParent(level2);
+            MessageBox.Show(level1.GetType().ToString());
+        }
     }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
 }
