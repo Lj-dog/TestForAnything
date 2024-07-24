8474cec11b691b456edf6de81e649b93b0496213
Luojieyin <luojieyinoffical@126.com>
2024-07-24 15:15:08 +0800

    ConsoleApp项目函数形参跳过默认参数给定

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");\
+
+            //(1)
+            //Print(0, num3: 5);
+
+        }
+
+        //(1)函数形参跳过默认参数给定
+        static void Print(int num1,int num2 =2,int num3=3)
+        {
+            Console.WriteLine($"num1:{num1},num2:{num2},num3:{num3}");
         }
     }
 }
