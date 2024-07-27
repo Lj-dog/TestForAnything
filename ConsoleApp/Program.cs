28616ce75f044c956c3f92dfa993e2984d521f8d
Luojieyin <luojieyinoffical@126.com>
2024-07-27 17:41:59 +0800

    TCPServer

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static  void Main(string[] args)
         {
             //Console.WriteLine("Hello, World!");\
 
@@ -11,16 +11,27 @@
 
             //(2)
 
-
+            //(3)异步方法
+            //int a;
+            //Task.Run(() => {
+            //    a = await Sum(1, 4);
+            //});
+            //int a = await Sum(1, 4);
+            //Console.WriteLine(a);
         }
 
         //(1)函数形参跳过默认参数给定
-        static void Print(int num1,int num2 =2,int num3=3)
+        static void Print(int num1, int num2 = 2, int num3 = 3)
         {
             Console.WriteLine($"num1:{num1},num2:{num2},num3:{num3}");
         }
 
-        
+        //(3)异步方法
+        public static async Task<int> Sum(int a, int b)
+         {
+            await Task.Delay(1);
+            return a + b;
+        }
     }
 
     //(2) 委托解耦调用
