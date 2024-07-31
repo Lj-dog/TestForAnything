758c474dea09d74d17c3d516ced4832fcdd306d8
Luojieyin <luojieyinoffical@126.com>
2024-07-31 20:20:29 +0800

    测试增加 不定参数及 数据类型转换。

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,8 +1,45 @@
 ﻿namespace ConsoleApp
 {
+    //抽象类里使用虚方法
+
+    #region MyRegion
+
+    internal abstract class People
+    {
+        public virtual void Age()
+        {
+            Console.WriteLine($"teenager");
+        }
+
+        public abstract void Name(string name);
+    }
+
+    internal class Student : People
+    {
+        public override void Name(string name)
+        {
+            Console.WriteLine($"I'm student {name}");
+        }
+    }
+
+    internal class Teacher : People
+    {
+        public override void Age()
+        {
+            Console.WriteLine($"Adult");
+        }
+
+        public override void Name(string name)
+        {
+            Console.WriteLine($"I'm teacher {name}");
+        }
+    }
+
+    #endregion MyRegion
+
     internal class Program
     {
-        static  void Main(string[] args)
+        private static void Main(string[] args)
         {
             //Console.WriteLine("Hello, World!");\
 
@@ -18,26 +55,46 @@
             //});
             //int a = await Sum(1, 4);
             //Console.WriteLine(a);
+
+            //(4) ushrot shrot转换(不丢失任何数据)
+            //short sh = -2;
+            //ushort ush = (ushort)sh;
+            //Console.WriteLine($"{(short)ush}");
+
+            //(5) 显示调用拥有不定参数和默认参数的函数
+            TestParams(bools: [true, false]);
+            TestParams(2, bools: [false, true]);
         }
 
         //(1)函数形参跳过默认参数给定
-        static void Print(int num1, int num2 = 2, int num3 = 3)
+        private static void Print(int num1, int num2 = 2, int num3 = 3)
         {
             Console.WriteLine($"num1:{num1},num2:{num2},num3:{num3}");
         }
 
         //(3)异步方法
         public static async Task<int> Sum(int a, int b)
-         {
+        {
             await Task.Delay(1);
             return a + b;
+        }
+
+        //(5) 显示调用拥有不定参数和默认参数的函数
+        public static void TestParams(int a = 1, params bool[] bools)
+
+        {
+            Console.WriteLine($"{a}");
+            foreach (var b in bools)
+            {
+                Console.WriteLine($"{b}");
+            }
         }
     }
 
     //(2) 委托解耦调用
 
-    class ClassPrint
+    internal class ClassPrint
     {
-        public  delegate void Print(string str);
+        public delegate void Print(string str);
     }
 }
---
+++ ConsoleApp_LINQ/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp_LINQ
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Dictionary<string, string> dict = new Dictionary<string, string>() {
+                {"asdf_1","110" },{"qwer1","5"},{"asdf_2","111"},{"qwer2","5"},
+            };
+            var dictlist = dict.Keys.Where(key => key.StartsWith("asdf_"));
+
+            foreach (var key in dictlist)
+            {
+                Console.WriteLine(key);
+            }
+        }
+    }
+}
