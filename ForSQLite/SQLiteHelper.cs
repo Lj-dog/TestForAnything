57b14894f767323fbdb5f2c548e07ca0fa7fa7d9
Luojieyin <luojieyinoffical@126.com>
2024-08-05 19:09:15 +0800

    JSON 序列化 特性（排序特性）。增加ConsoleTemp工程用于临时测试示例

---
+++ ForSQLite/SQLiteHelper.cs
@@ -0,0 +1,46 @@
+﻿using FreeSql.DataAnnotations;
+using System;
+using FreeSql;
+
+namespace ForSQLite
+{
+    public class SQLiteHelper
+    {
+        private static IFreeSql fsql;
+
+        private Type dbTabletype;
+
+        public SQLiteHelper(string dbFilePath, bool isAutoAddTableOrFile/*, Type dbTabletype*/)
+        {
+            //this.dbTabletype = dbTabletype;
+            try
+            {
+                fsql = new FreeSql.FreeSqlBuilder().UseConnectionString(FreeSql.DataType.Sqlite, "Data Source=" + dbFilePath)
+                              .UseAutoSyncStructure(isAutoAddTableOrFile) //自动同步实体结构到数据库
+                                  .Build(); //请务必定义成 Singleton 单例模式
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("路径下不存在该数据库");
+            }
+        }
+
+        public int LoadToDB(object dbData)
+        {
+            //if (dbData.GetType()!= dbTabletype)
+            //{
+            //    return false;
+            //}
+            try
+            {
+                var row = fsql.Insert(dbData)
+                                .ExecuteAffrows();
+                return row;
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+    }
+}
