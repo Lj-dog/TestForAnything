16dd637de958035d9acf650bd008dd0cc525204b
Luojieyin <luojieyinoffical@126.com>
2024-08-30 11:04:59 +0800

    (迁移子项目进来前)添加Mvvm测试程序，切换窗体程序。

---
+++ WpfAppMVVMTest/ViewModels/DataGridVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WpfAppMVVMTest.ViewModels
+{
+    class DataGridVM : ObservableObject
+    {
+        public 
+        public DataGridVM()
+        {
+            
+        }
+    }
+}
