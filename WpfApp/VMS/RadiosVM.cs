772355e563ec303db3ff3ac154c68b723d9e00be
Lj <luojieyinoffical@126.com>
2025-03-16 21:38:13 +0800

    RaidoButton 在同一个布局控件无法多选

---
+++ WpfApp/VMS/RadiosVM.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.VMS
+{
+    public partial class RadiosVM: ObservableObject
+    {
+        static bool switchChecked = false;
+
+        [ObservableProperty]
+        private bool isChecked_1;
+
+        [ObservableProperty]
+        private bool isChecked_2;
+
+        [RelayCommand]
+        private void Checked()
+        {
+            if(switchChecked)
+            {
+                switchChecked = false;
+                IsChecked_1 = true;
+                IsChecked_2 = true;
+            }
+            else
+            {
+                switchChecked = true;
+                IsChecked_2 = true;
+                IsChecked_1 = true;
+            }
+        }
+    }
+}
--- WpfAppMVVMTest/ViewModels/RadioVM.cs
+++ WpfAppMVVMTest/ViewModels/RadioVM.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,5 +15,7 @@ namespace WpfAppMVVMTest.ViewModels
 
         [ObservableProperty]
         private bool checked2;
+
+        ObservableCollection<bool> checkList;
     }
 }
