9a1ee582551adebe3be10c140f98151bc41e5cc6
Lj <luojieyinoffical@126.com>
2025-05-26 10:32:10 +0800

    两异步方法同时调用static异步方法

--- ConsoleAppTemp/Program.cs
+++ ConsoleAppTemp/Program.cs
@@ -1,12 +1,17 @@
-﻿using FreeSql.DataAnnotations;
-using System;
-using System.Reflection;
-
-using FreeSql;
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
 using System.Drawing;
 using System.Globalization;
-using System.Collections;
+using System.Linq.Expressions;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using FreeSql;
+using FreeSql.DataAnnotations;
 
 namespace ConsoleAppTemp
 {
@@ -14,8 +19,6 @@ namespace ConsoleAppTemp
     {
         private static void Main(string[] args)
         {
-
-
             #region 正则表达式
             //string pattern = @"(QX|IX)(\d{1,4})\.([0-7])$";
             //  Regex regex = new Regex(pattern);
@@ -30,12 +33,16 @@ namespace ConsoleAppTemp
             #endregion
             #region 异常Data属性的使用
 
-            Console.WriteLine("\nException with some extra information...");
-            RunTest(false);
-            Console.WriteLine("\nException with all extra information...");
-            RunTest(true); 
+            //Console.WriteLine("\nException with some extra information...");
+            //RunTest(false);
+            //Console.WriteLine("\nException with all extra information...");
+            //RunTest(true);
             #endregion
+            Machine machine1 = new(1, "machine1");
+            Machine machine2 = new(2, "machine2");
 
+            machine1.Run();
+            machine2.Run();
         }
 
         #region 异常Data属性的使用
@@ -53,8 +60,11 @@ namespace ConsoleAppTemp
                 {
                     Console.WriteLine("  Extra details:");
                     foreach (DictionaryEntry de in e.Data)
-                        Console.WriteLine("    Key: {0,-20}      Value: {1}",
-                                          "'" + de.Key.ToString() + "'", de.Value);
+                        Console.WriteLine(
+                            "    Key: {0,-20}      Value: {1}",
+                            "'" + de.Key.ToString() + "'",
+                            de.Value
+                        );
                 }
             }
         }
@@ -86,7 +96,164 @@ namespace ConsoleAppTemp
                 e.Data["DateTimeInfo"] = dt;
             }
             throw e;
-        } 
+        }
+        #endregion
+
+        #region 两异步方法同时调用static异步方法
+
+        //模拟IOC拿到实例
+        public static ServerManager Server =
+            new("502", new List<string> { "127.0.0.1", "192.168.3.4" });
+
+        public static WorkFlow workFlow = new();
+
+        public class ServerManager
+        {
+            private TcpListener TcpListener;
+
+            public List<string> IPList { get; set; }
+
+            public Dictionary<string, bool> IsConnected { get; private set; }
+
+            public ConcurrentDictionary<string, TcpClient> Clients { get; private set; } = new ConcurrentDictionary<string, TcpClient>();
+
+            public ConcurrentDictionary<string, bool> IsReply { get; set; } = new ConcurrentDictionary<string, bool>();
+
+            public ServerManager(string port, List<string> iplist)
+            {
+                IPList = iplist;
+                TcpListener = new(IPAddress.Any, int.Parse(port));
+            }
+
+            public async Task StartAsync()
+            {
+                TcpListener.Stop();
+                TcpListener.Start();
+                while (true)
+                {
+                    TcpClient client = await TcpListener.AcceptTcpClientAsync();
+
+                    var ip = ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString();
+                    if (IPList.Contains(ip))
+                    {
+                        Clients.AddOrUpdate(ip, client, (key, c) => { return client; });
+                        IsConnected[ip] = true;
+                        _ = HandleAsync(client,ip);
+                    }
+                }
+            }
+
+            private async Task HandleAsync(TcpClient client,string ip)
+            {
+                NetworkStream stream = client.GetStream();
+                var data = new byte[1024];
+                while (true)
+                {
+                    try
+                    {
+                        int datanum = await stream.ReadAsync(data, 0, data.Length);
+                        if (datanum == 0)
+                            break;
+                        IsReply[ip] = true;
+                    }
+                    catch (Exception e) when (e is IOException || e is SocketException)
+                    {
+
+                        break;
+                    }
+                }
+                Clients.TryRemove(ip, out var c);
+                IsConnected[ip] = false;
+                stream.Close();
+                client.Close();
+            }
+
+            public async Task SendAsync(string ip,string message)
+            {
+                TcpClient client;
+                if (!Clients.TryGetValue(ip, out client))
+                    return;
+                NetworkStream network = client.GetStream();
+                try
+                {
+                    var messageBytes = Encoding.UTF8.GetBytes(message);
+                    await network.WriteAsync(messageBytes, 0, messageBytes.Length);
+
+                }
+                catch (Exception e) when (e is IOException || e is SocketException)
+                {
+
+                    Clients.TryRemove(ip, out var _);
+                    IsConnected[ip] = false;
+                    network.Dispose();
+                    client.Close();
+                }
+            }
+        }
+
+        public static async Task<bool> IsSendSuccess(byte machineNum, string message)
+        {
+            var ip = Server.IPList[machineNum];
+            if (!Server.IsConnected[ip])
+                return false;
+            await Server.SendAsync(ip, message);
+            await Task.Delay(500);//waiting reply
+            for (int i = 0; i < 3; i++)
+            {
+                var res = Server.IsReply[ip];
+                if (res)
+                {
+                    Server.IsReply[ip] = false;
+                    return res;
+                }
+                await Task.Delay(100); // waiting again
+            }
+            return false;
+        }
+
+        public class Machine
+        {
+            private byte num;
+            //携带信息
+            public string Name { get; set; }
+            public Machine(byte num ,string name)
+            {
+                this.num = num;
+                Name = name;
+            }
+            public async void Run()
+            {
+                try
+                {
+                    var token = new CancellationTokenSource();
+                    while (true)
+                    {
+                        workFlow.Working(num, IsSendSuccess, token.Token);
+                    }
+                }
+                catch (Exception)
+                {
+
+                    ;
+                }
+                //...
+            }
+            //...
+        }
+
+        //在另一个外部库
+        public class WorkFlow
+        {
+            public async void Working(byte machineNum, Func<byte, string, Task<bool>> isSendSuccess,CancellationToken token)
+            {
+                //其他操作 异步
+                //
+                var message = "其他设备上拿到的Message";
+                if (!await IsSendSuccess(machineNum, message))
+                    return;
+                //其他操作 异步
+            }
+        }
         #endregion
     }
 }
