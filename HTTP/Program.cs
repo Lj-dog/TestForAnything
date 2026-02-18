e56d56b44e631de3d3bc027d519d8dbdb8ceea55
luojieyin <luojieyinoffical@126.com>
2026-02-18 13:25:22 +0800

    完成http对于httpbin.org网站基础http方法，密码验证，图片资源下载测试

---
+++ HTTP/Program.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using HTTP.Services;
+namespace HTTP
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+            builder.Services.AddHttpClient<HttpBinService>(client =>
+            {
+                client.BaseAddress = new Uri("https://httpbin.org/");
+            });
+            builder.Services.AddLogging(configure => configure.AddConsole());
+
+            var provider = builder.Build().Services;
+
+
+            var httpBinService= provider.GetRequiredService<HttpBinService>();
+
+            ILogger logger = provider.GetRequiredService<ILogger<Program>>();
+
+            logger.LogInformation("Get");
+            var getRes = await httpBinService.GetIPAsync();
+
+            logger.LogInformation("Get Url:{Url} \n\r ", getRes.Url);
+
+            //
+
+            logger.LogInformation("Delete");
+            var deleteRes = await httpBinService.DeleteAsync();
+            logger.LogInformation("Delete Url:{Url} \n\r ", deleteRes.Url);
+
+            //
+            logger.LogInformation("Patch");
+            var patchRes = await httpBinService.PatchAsync("patch method");
+            logger.LogInformation("Patch Url:{Url} \n\r ", patchRes.Url);
+
+            //
+            logger.LogInformation("Post");
+            var postRes = await httpBinService.PostAsync("post method");
+            logger.LogInformation("Post Url:{Url} \n\r ", postRes.Url);
+
+            //
+            logger.LogInformation("Put");
+            var putRes = await httpBinService.PutAsync("put method");
+            logger.LogInformation("Put Url:{Url} \n\r ", putRes.Url);
+
+            //
+            logger.LogInformation("Get Auth");
+            var getAuthRes = await httpBinService.GetAuthAsync("asdf", "123");
+            logger.LogInformation("User : {user} Get authenticated : {result} \n\r ",getAuthRes.User, getAuthRes.Authenticated);
+        
+            //
+            logger.LogInformation("Get Deny Robot.Txt");
+            var getDenyRobotTxtRes = await httpBinService.GetDenyRobotTxtAsync();
+            logger.LogInformation(getDenyRobotTxtRes + "\n\r");
+
+            //
+            logger.LogInformation("Get image");
+            await httpBinService.GetImageAsync("jpeg");
+            await httpBinService.GetImageAsync("png");
+            await httpBinService.GetImageAsync("webp");
+            logger.LogInformation("Get image successfully \n\r");
+        }
+    }
+}
